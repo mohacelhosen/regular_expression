9748599b38c2275ade31e82c31ee4a5c221493e3
mohacelhosen <mohacel.hosen22@gmail.com>
2023-07-14 11:07:48 +0600

    [ ]matching any single character within the brackets

--- src/main/java/org/example/SquareBrackets.java
+++ src/main/java/org/example/SquareBrackets.java
@@ -12,10 +12,15 @@ import java.util.regex.Pattern;
  **/
 public class SquareBrackets {
     public static void main(String[] args) {
-        String regex = "[abc]"; // search single character  either 'a' or 'b' or 'c'
+        String regex = "[abc]"; // search any single character either 'a' or 'b' or 'c'
+        String exceptRegex = "[^abc]"; // search any single character except 'a' or 'b' or 'c'
+        String exceptAlphabetRegex = "[^a-zA-Z]"; // search any single character except alphabet
+        String exceptDigitRegex = "[^0-9]"; // search any single character except digit
+        String exceptAlphaNumericRegex = "[^a-zA-Z0-9]"; // search any single character except AlphaNumeric
+        String alphaNumericRegex = "[a-zA-Z0-9]"; // search any single character of AlphaNumeric
         String input = "$b@c709a";
 
-        Pattern pattern= Pattern.compile(regex);
+        Pattern pattern= Pattern.compile(alphaNumericRegex);
         Matcher matcher = pattern.matcher(input);
         while (matcher.find()){
             System.out.println("Start index::"+matcher.start()+"  Matching String::"+matcher.group());
